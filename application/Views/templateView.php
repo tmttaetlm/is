ea01d9b42195f4f0952ad33a8e5dc9eda6ddc78d
Vladimir Martynenko <vlmart@bk.ru>
2019-09-27 17:08:56 +0600

    111

--- application/Views/templateView.php
+++ application/Views/templateView.php
@@ -3,7 +3,7 @@
     <head>
         <title>Информационные системы NIS Kostanay</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="/public/css/style_ver.1.6.css">
+        <link rel="stylesheet" type="text/css" href="/public/css/style_ver.1.7.css">
     </head>
     <body>
         <header>
@@ -29,6 +29,6 @@
         <footer>
         </footer>
         <p class="copyright">Copyright © 2019 NIS Kostanay</p>
-        <script type="module" src="/public/js/script_ver.1.6.js"></script>
+        <script type="module" src="/public/js/script_ver.1.7.js"></script>
     </body>
 </html>
