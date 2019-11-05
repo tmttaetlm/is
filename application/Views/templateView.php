217aff763335a45d47f41d67b0556ab64b800ea6
Vladimir Martynenko <vlmart@bk.ru>
2019-11-05 10:31:29 +0600

    `

--- application/Views/templateView.php
+++ application/Views/templateView.php
@@ -29,7 +29,7 @@
         <footer>
         <p class="copyright">Copyright © 2019 NIS Kostanay</p>
         </footer>
-        <script src="/public/js/script_ver.2.5.js"></script>
+        <script src="/public/js/script_ver.2.6.js"></script>
         
     </body>
 </html>
