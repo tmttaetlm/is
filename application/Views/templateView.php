5dccb36c7c0e966b0b9b89a8bd552cacdf20da3b
Medet <turegeldin_m@kst.nis.edu.kz>
2020-11-05 16:30:10 +0600

    icon

--- application/Views/templateView.php
+++ application/Views/templateView.php
@@ -4,6 +4,14 @@
         <title>Информационные системы NIS Kostanay</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="/public/css/style_ver.3.3.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/images/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/images/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/images/icons/favicon-16x16.png">
+        <link rel="manifest" href="/public/images/icons/site.webmanifest">
+        <link rel="mask-icon" href="/public/images/icons/safari-pinned-tab.svg" color="#5e852c">
+        <meta name="msapplication-TileColor" content="#365908">
+        <meta name="theme-color" content="#678f33">
     </head>
     <body>
         <header>
@@ -27,8 +35,8 @@
 <?php echo $data['content']; ?>
        
         <footer>
-        <p class="copyright">Copyright © 2019 NIS Kostanay</p>
+        <p class="copyright">Copyright © <?php echo date('Y') ?> NIS Kostanay</p>
         </footer>
-        <script type="module" src="/public/js/script_ver.3.3.25.js"></script>
+        <script type="module" src="/public/js/script_ver.3.4.1.js"></script>
     </body>
 </html>
