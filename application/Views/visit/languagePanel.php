8402faa196eef7419fd142c09bfb7f9472b9cde4
Medet <turegeldin_m@kst.nis.edu.kz>
2021-03-16 11:06:36 +0600

    notification

--- application/Views/visit/languagePanel.php
+++ application/Views/visit/languagePanel.php
@@ -1,7 +1,3 @@
-<div id="custom-notification" class="custom-notification-block">
-    <div id="custom-notification-text" class="custom-notification notification-hide">
-    </div> 
-</div>
 <div class="language-panel">
     <label>Язык листа наблюдения: </label>
     <input type="radio" name="lang" id="kaz-lang">
